bc956ae8d8de90ce303cf6797c257b79954f7537
Alan Palazzolo <apalazzolo@minnpost.com>
2013-03-28 16:02:29 -0500

    Parsing out JSON a bit.

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,7 @@ from fabric.api import *
 import requests
 import pprint
 import boto
+import json
 
 
 """
@@ -29,10 +30,28 @@ def cache_google_spreadsheet_to_s3(key = ''):
   """
   meta_url = 'https://spreadsheets.google.com/feeds/worksheets/' + key + '/public/basic?alt=json-in-script'
   r = requests.get(meta_url)
+  meta_jsonp_text = r.text
+  
+  # Get meta JSON
+  jsonp_prefix = 'gdata.io.handleScriptLoaded('
+  jsonp_suffix = ');'
+
+  if meta_jsonp_text.startswith(jsonp_prefix) and meta_jsonp_text.endswith(jsonp_suffix):
+    meta_json_text = meta_jsonp_text[len(jsonp_prefix): -len(jsonp_suffix)]
+  else:
+    print 'JSONP not in correct format.'
+    return
+    
+  meta = json.loads(meta_json_text)
   
   # Copy to S3
   s3 = boto.connect_s3()
   bucket = s3.create_bucket(env.s3_bucket)
+  
+  
+  #key = bucket.new_key('examples/first_file.csv')
+  #key.set_contents_from_filename('/home/patrick/first_file.csv')
+  #key.set_acl('public-read')
 
 
 def test_env():
